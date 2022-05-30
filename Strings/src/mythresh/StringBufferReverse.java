eef5b9bddf94175abd3912b4f3819b00affa74e7
Udatha <mythresh.udatha@capgemini.com>
2022-05-30 19:28:16 +0530

    codes

---
+++ Strings/src/mythresh/StringBufferReverse.java
@@ -0,0 +1,9 @@
+package mythresh;
+
+public class StringBufferReverse {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This method returns the reversed object on which it was called");
+		System.out.println(sb.reverse());
+		
+	}
+}
---
+++ Strings/src/mythresh/StringLength.java
@@ -0,0 +1,8 @@
+package mythresh;
+
+public class StringLength {
+	public static void main(String[] args) {
+		String str = "Hello World";
+		System.out.println("Length of the String is: " + str.length());
+	}
+}
