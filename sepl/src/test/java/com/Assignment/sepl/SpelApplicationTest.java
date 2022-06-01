b647abd8eff165c6efd4112be90b7240a1572cf8
Udatha <mythresh.udatha@capgemini.com>
2022-06-01 17:09:43 +0530

    code

---
+++ sepl/src/test/java/com/Assignment/sepl/SpelApplicationTest.java
@@ -0,0 +1,14 @@
+package com.Assignment.sepl;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
